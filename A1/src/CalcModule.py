fc96d3a5e388a13933e0434bc925f0ed3a4ae5d6
unknown <wengyunbing@gmail.com>
2019-01-30 00:44:02 -0500

    almost done, hate windows sometines

--- A1/src/CalcModule.py
+++ A1/src/CalcModule.py
@@ -1,5 +1,77 @@
 ## @file CalcModule.py
-#  @author 
-#  @brief 
-#  @date 
+#  @author janzej2
+#  @brief Three functions for calculating/allocating students to their chosen programs.
+#  @date 01/17/19
 
+#import Read AllocationData as well as operator (used for sorting list of dictionaries)
+import operator
+from ReadAllocationData import *
+
+## @brief This function sorts a list of dictionaries from lowest to highest GPAs
+# @param S A list of students, each represented as a dictionary.
+# @return A list of students, in order from highest to lowest GPAs.
+#sort function inspired by https://www.geeksforgeeks.org/ways-sort-list-dictionaries-values-python-using-lambda-function/
+def sort(S):
+    return sorted(S, key = lambda i: i['gpa'], reverse=True)
+
+## @brief This function finds the average GPA of a set of students (based on gender).
+# @param L A list of dictionaries created by readStdnts(s).
+# @param g A string representing the gender ("male" or "female").
+# @return The average GPA of male or female students as a float.
+def average(L, g):
+    average = 0
+    count = 0
+    for i in range(len(L)):
+        if L[i].get("gender") == g:
+            count += 1
+            average += L[i].get("gpa")
+    if count != 0:
+        return average/count
+    else:
+        return 0
+
+## @brief This function allocates students to their program choices based on a number of
+# factors.
+# @details The function takes a list of all students, students with free choice and a
+# dictionary containing the capacities of each department. If the student has free choice,
+# they are automatically added to their chosen program (it is assumed that there will always
+# be space in a given program for free choice students, see ReadAllocationData.py for full
+# assumption list) while otherwise, they are allocated based on capacity.
+# @param S A list of dictionaries of students, created by readStdnts().
+# @param F A list of students with free choice (created by readFreeChoice()).
+# @param C A dictionary of department capacities (created by readDeptCapacity()).
+# @return A dictionary formatted with the format 'program' : [student, student...]
+# for each potential program.
+def allocate(S, F, C):
+    civ, chem, elec, mech, soft, matls, engphys = ([] for i in range(7))
+    #create final dictionary to return/index in the future
+    final = {'civil' : civ, 'chemical' : chem, 'electrical' : elec, 'mechanical' : mech, 'software' : soft, 'materials' : matls, 'engphys' : engphys}
+    #allocate free choice students first
+    for i in range(len(F)):
+        for j in range(len(S)):
+            #find macid in student list and take index position
+            if F[i] == S[j].get("macid"):
+                break
+        #add student to first choice program
+        (final.get((S[j].get("choices"))[0])).append(S[i])
+        #remove student from main student list so they aren't allocated twice
+        del S[j]
+    S = sort(S)
+    #define previous GPA in case of duplicates
+    previous = -1
+    for i in range(len(S)):
+        #only allocate those with gpa greater than 4.0. see assumptions in ReadAllocationData.py
+        if S[i].get("gpa") < 4.0:
+            break
+        first = (S[i].get("choices"))[0]
+        second = (S[i].get("choices"))[1]
+        third = (S[i].get("choices"))[2]
+        #check capacities and allocate accordingly
+        if len(final.get(first)) < C.get(first) or (len(final.get(first)) == C.get(first) and previous == S[i].get('gpa')):
+            (final.get(first)).append(S[i])
+        elif len(final.get(second)) < C.get(second) or (len(final.get(second)) == C.get(second) and previous == S[i].get('gpa')):
+            (final.get(second)).append(S[i])
+        else:
+            (final.get(third)).append(S[i])
+        previous = S[i].get('gpa')
+    return final
--- A1/src/testCalc.py
+++ A1/src/testCalc.py
@@ -1,5 +1,69 @@
 ## @file testCalc.py
-#  @author 
-#  @brief 
-#  @date 
+#  @wengy12
+#  @brief
+#  @01/29/2019
 
+import random
+import CalcModule
+
+gen = ["male", "female"]
+choices = ["software", "civil", "chemical", "electrical", "mechanical",  "materials", "engphys"]
+testlst = [{"macid": 400158850, "fname": "Peter", "lname": "The Best", "gender": "male", "gpa": 12, "choice": [choices[0], choices[1], choices[2]]}]
+male_gpa = 12
+male_num = 1
+female_gpa = 0
+female_num = 0
+
+for i in range(1, 1000):
+    random.shuffle(choices)
+    prev_gpa = testlst[i-1].get("gpa")
+    if prev_gpa is not None:
+        prev_gpa = float(prev_gpa)
+        newstd = {"macid": 400158850+i, "fname": "liz("+str(i)+")", "lname": "goodman("+str(i)+")", "gender": gen[random.randint(0,1)], "gpa": (random.random()/500+0.998)*prev_gpa, "choice": [choices[0], choices[1], choices[2]]}
+        g = newstd.get("gender")
+        gp = newstd.get("gpa")
+        if g is not None and gp is not None:
+            if g == "male":
+                male_gpa += gp
+                male_num += 1
+            else:
+                female_gpa += gp
+                female_num += 1
+        testlst.append(newstd)
+
+male_gpa /= male_num
+female_gpa /= female_num
+
+newlst = testlst
+random.shuffle(newlst)
+CalcModule.sort(newlst)
+if newlst != testlst:
+    print("error in sorting")
+else:
+    print("sorting is good")
+
+random.shuffle(newlst)
+print("male average gpa is ", male_gpa)
+print("calculated male agpa is ", CalcModule.average(newlst, "male"))
+print("female average gpa is ", female_gpa)
+print("calculated female agpa is ", CalcModule.average(newlst, "female"))
+
+capacity = {"software": 150, "civil": 150, "chemical": 150, "electrical": 150, "mechanical": 150,  "materials": 150, "engphys": 150}
+free_choice = [400159849]
+res = CalcModule.allocate(newlst, free_choice, capacity)
+fc = 0
+now = 0
+for i in res:
+    for j in range(i):
+        if i[j].get("macid") == 400159849:
+            fc += 1
+            if (j != 0):
+                print("error")
+            continue
+        if now > i[j]:
+            print("error")
+        else:
+            now = i[j]
+    if len(i) > 150:
+        print("error")
+    now = 0
